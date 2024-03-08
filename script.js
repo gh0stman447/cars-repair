5bdf86191b1735f0bec103cc8ff8b0804701b52a
gh0stman447 <o_malygin@bk.ru>
2024-03-09 00:20:41 +0300

    added an adaptive header

--- script.js
+++ script.js
@@ -1,4 +1,28 @@
-function toggleMenu() {
-  let nav = document.getElementById('nav');
-  nav.style.display = nav.style.display === 'flex' ? 'none' : 'flex';
-}
+document.addEventListener('DOMContentLoaded', function () {
+  const nav = document.getElementById('nav');
+  const burgerMenu = document.querySelector('.burger-menu');
+  const cross = document.querySelector('.cross');
+  const screenWidthThreshold = 730;
+
+  window.addEventListener('resize', function () {
+    if (window.innerWidth > screenWidthThreshold) {
+      burgerMenu.classList.add('hidden');
+    } else {
+      burgerMenu.classList.remove('hidden');
+    }
+  });
+
+  function toggleNav() {
+    nav.classList.toggle('invisibleNav');
+    burgerMenu.classList.toggle('hidden');
+    cross.classList.toggle('hidden');
+  }
+
+  burgerMenu.addEventListener('click', function () {
+    toggleNav();
+  });
+
+  cross.addEventListener('click', function () {
+    toggleNav();
+  });
+});
